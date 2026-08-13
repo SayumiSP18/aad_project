205f2f0a31b086273fb681e5aa24f76ce134df9f
SayumiPerera <sayumiperera18@gmail.com>
2026-08-13 11:12:07 +0530

    first commit

---
+++ src/main/java/com/example/aad_project/AadProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.aad_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadProjectApplication.class, args);
+	}
+
+}
