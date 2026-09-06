219ff0fa793b182adbba68c1df5393dce78b83c8
SayumiPerera <sayumiperera18@gmail.com>
2026-09-06 23:14:42 +0530

    delivery service  creation

--- src/main/java/com/example/aad_project/service/DeliveryService.java
+++ src/main/java/com/example/aad_project/service/DeliveryService.java
@@ -1,4 +1,19 @@
 package com.example.aad_project.service;
 
+import com.example.aad_project.dto.DeliveryDTO;
+
+import java.util.List;
+
 public interface DeliveryService {
+    void saveDelivery(DeliveryDTO deliveryDTO);
+
+    List<DeliveryDTO> getAllDeliveries();
+
+    List<DeliveryDTO> filterDeliveries(Long driverId);
+
+    DeliveryDTO selectDelivery(long deliveryId);
+
+    void updateDelivery(DeliveryDTO deliveryDTO);
+
+    void deleteDelivery(long deliveryId);
 }
