1c1e4e4257215d716fc1de45f21678be2735f317
SayumiPerera <sayumiperera18@gmail.com>
2026-09-06 23:04:41 +0530

    invoice,delivery dto create

---
+++ src/main/java/com/example/aad_project/dto/DeliveryDTO.java
@@ -0,0 +1,51 @@
+package com.example.aad_project.dto;
+
+import com.example.aad_project.enumaration.ParcelStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryDTO {
+
+    private long deliveryId;
+
+//    @NotNull(message = "Parcel is required")
+    private Long parcelId;
+
+    private String trackingNo;
+
+//    @NotNull(message = "Driver is required")
+    private Long driverId;
+
+    private String driverUsername;
+
+//    @NotNull(message = "Vehicle is required")
+    private Long vehicleId;
+
+    private String vehicleNo;
+
+    private Long routeId;
+
+    private ParcelStatus status;
+
+    private LocalDateTime deliveredAt;
+
+    public DeliveryDTO(long deliveryId, long parcelId, String trackingNo, long driverId, String driverUsername,
+                       long vehicleId, String vehicleNo, Long routeId, ParcelStatus status, LocalDateTime deliveredAt) {
+        this.deliveryId = deliveryId;
+        this.parcelId = parcelId;
+        this.trackingNo = trackingNo;
+        this.driverId = driverId;
+        this.driverUsername = driverUsername;
+        this.vehicleId = vehicleId;
+        this.vehicleNo = vehicleNo;
+        this.routeId = routeId;
+        this.status = status;
+        this.deliveredAt = deliveredAt;
+    }
+}
--- src/main/java/com/example/aad_project/dto/InvoiceDTO.java
+++ src/main/java/com/example/aad_project/dto/InvoiceDTO.java
@@ -1,4 +1,27 @@
 package com.example.aad_project.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InvoiceDTO {
-}
+
+    private long invoiceId;
+
+//    @NotNull(message = "Payment is required")
+    private Long paymentId;
+
+    private String invoiceNo;
+    private LocalDate issuedDate;
+
+    public InvoiceDTO(long invoiceId, long paymentId, String invoiceNo, LocalDate issuedDate) {
+        this.invoiceId = invoiceId;
+        this.paymentId = paymentId;
+        this.invoiceNo = invoiceNo;
+        this.issuedDate = issuedDate;
+    }}
