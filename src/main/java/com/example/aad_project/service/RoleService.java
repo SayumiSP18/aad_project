1f52be72d58ec41809f4f947deb21769d07af272
SayumiPerera <sayumiperera18@gmail.com>
2026-09-06 22:48:53 +0530

    role service update

--- src/main/java/com/example/aad_project/service/RoleService.java
+++ src/main/java/com/example/aad_project/service/RoleService.java
@@ -2,8 +2,17 @@ package com.example.aad_project.service;
 
 
 
+import com.example.aad_project.dto.RoleDTO;
+
 import java.util.List;
 
 public interface RoleService {
 
+    void saveRole(RoleDTO roleDTO);
+
+    List<RoleDTO> getAllRoles();
+
+    RoleDTO selectRole(long roleId);
+
+    void deleteRole(long roleId);
 }
