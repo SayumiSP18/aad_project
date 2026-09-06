83f675180c29643a8e5b86f92151a5ba0c2b7662
SayumiPerera <sayumiperera18@gmail.com>
2026-09-06 23:11:59 +0530

    invoice service  creation

--- src/main/java/com/example/aad_project/service/InvoiceService.java
+++ src/main/java/com/example/aad_project/service/InvoiceService.java
@@ -1,4 +1,19 @@
 package com.example.aad_project.service;
 
+import com.example.aad_project.dto.InvoiceDTO;
+
+import java.util.List;
+
 public interface InvoiceService {
+    void saveInvoice(InvoiceDTO invoiceDTO);
+
+    List<InvoiceDTO> getAllInvoices();
+
+    List<InvoiceDTO> filterInvoices(String invoiceNo);
+
+    InvoiceDTO selectInvoice(long invoiceId);
+
+    void updateInvoice(InvoiceDTO invoiceDTO);
+
+    void deleteInvoice(long invoiceId);
 }
