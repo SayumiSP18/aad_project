ae15cc7aa7af0d4a0ec1a15694ab6eefabe02d86
SayumiPerera <sayumiperera18@gmail.com>
2026-09-06 22:55:33 +0530

    booking repo create

--- src/main/java/com/example/aad_project/repository/BookingRepository.java
+++ src/main/java/com/example/aad_project/repository/BookingRepository.java
@@ -1,4 +1,30 @@
 package com.example.aad_project.repository;
 
-public interface BookingRepository {
+import com.example.aad_project.dto.BookingDTO;
+import com.example.aad_project.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    Optional<Booking> findByParcel_ParcelId(long parcelId);
+
+    @Query(value = "SELECT new com.example.aad_project.dto.BookingDTO(b.bookingId, b.parcel.parcelId, b.parcel.trackingNo, " +
+            "b.pickupBranch.branchId, b.pickupBranch.name, b.bookingDate, b.estimatedCost) FROM Booking b")
+    List<BookingDTO> getAllBookings();
+
+    @Query(value = "SELECT new com.example.aad_project.dto.BookingDTO(b.bookingId, b.parcel.parcelId, b.parcel.trackingNo, " +
+            "b.pickupBranch.branchId, b.pickupBranch.name, b.bookingDate, b.estimatedCost) FROM Booking b WHERE b.bookingId = :bookingId")
+    Optional<BookingDTO> selectBooking(@Param("bookingId") long bookingId);
+
+    @Query(value = "SELECT new com.example.aad_project.dto.BookingDTO(b.bookingId, b.parcel.parcelId, b.parcel.trackingNo, " +
+            "b.pickupBranch.branchId, b.pickupBranch.name, b.bookingDate, b.estimatedCost) FROM Booking b " +
+            "WHERE (:pickupBranchId IS NULL OR b.pickupBranch.branchId = :pickupBranchId)")
+    List<BookingDTO> filterBookings(@Param("pickupBranchId") Long pickupBranchId);
 }
