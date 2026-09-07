79ec6fc34788481d4876551919a20de6d4ddcbc1
SayumiPerera <sayumiperera18@gmail.com>
2026-09-07 22:48:02 +0530

    tracking histrory controller

---
+++ src/main/java/com/example/aad_project/controller/TrackingHistoryController.java
@@ -0,0 +1,44 @@
+package com.example.aad_project.controller;
+
+import com.example.aad_project.constant.CommonResponse;
+import com.example.aad_project.dto.TrackingHistoryDTO;
+import com.example.aad_project.service.TrackingHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "v1/tracking-history")
+@CrossOrigin
+@RequiredArgsConstructor
+public class TrackingHistoryController {
+
+    private final TrackingHistoryService trackingHistoryService;
+
+    @PostMapping(value = "/save", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse saveHistory( @RequestBody TrackingHistoryDTO historyDTO) {
+        trackingHistoryService.saveHistory(historyDTO);
+        return new CommonResponse(0, "Tracking update recorded successfully");
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse getAllHistory() {
+        List<TrackingHistoryDTO> history = trackingHistoryService.getAllHistory();
+        return new CommonResponse(0, history, "Get all tracking history");
+    }
+
+    @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse filterHistory(@RequestParam(value = "parcelId", required = false) Long parcelId) {
+        List<TrackingHistoryDTO> history = trackingHistoryService.filterHistory(parcelId);
+        return new CommonResponse(0, history, "Filter tracking history");
+    }
+
+    @GetMapping(value = "/{historyId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CommonResponse selectHistory(@PathVariable long historyId) {
+        TrackingHistoryDTO dto = trackingHistoryService.selectHistory(historyId);
+        return new CommonResponse(0, dto, "Tracking record details");
+    }
+
+    }
